b115b098b7a91c965cd76dce033f02f27edd784e
motephyr <motephyr@gmail.com>
2014-11-23 20:22:56 +0800

    the map's permission setting

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -2,10 +2,10 @@ module MapsHelper
 
   def render_user_role_for_map(map)
 
-    if current_user
-      role = if Map.with_role(:manager, current_user).present?
+    if current_user && is_existed_map(map)      
+      role = if Map.with_role(:manager, current_user).pluck(:id).include?(map.id)
         "你是地圖管理者"
-      elsif Map.with_role(:invitee, current_user).present?
+      elsif Map.with_role(:invitee, current_user).pluck(:id).include?(map.id)
         "你是共同編輯者"
       else
         "你是一般使用者"
@@ -14,10 +14,14 @@ module MapsHelper
       "你是一般使用者"
     end
 
-    if current_user && map.present? 
+    if current_user && is_existed_map(map)
       "地圖名稱：#{@map.title} (#{role})"            
     else  
       "地圖列表"
     end
+  end
+
+  def is_existed_map(map)
+    map.present? && map.id?
   end
 end
