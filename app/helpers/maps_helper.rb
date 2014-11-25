600e5ff73aea711f4cd1162fec1415ab0cbce18d
motephyr <motephyr@gmail.com>
2014-11-26 00:20:19 +0800

    fix menu

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,7 +1,6 @@
 module MapsHelper
 
   def render_user_role_for_map(map)
-
     if current_user && is_existed_map(map)      
       role = if Map.with_role(:manager, current_user).pluck(:id).include?(map.id)
         "你是地圖管理者"
@@ -13,9 +12,12 @@ module MapsHelper
     else
       "你是一般使用者"
     end
+  end
+
+  def render_map_title_for_map(map)
 
     if current_user && is_existed_map(map)
-      "地圖名稱：#{@map.title} (#{role})"            
+      "地圖名稱：#{@map.title}"            
     else  
       "地圖列表"
     end
