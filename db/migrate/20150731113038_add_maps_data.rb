15d408c70698af9d9a0b743d97e22d571a671334
motephyr <motephyr@gmail.com>
2015-08-05 19:04:28 +0800

    remove create map date from migrate, fix invite.

--- db/migrate/20150731113038_add_maps_data.rb
+++ db/migrate/20150731113038_add_maps_data.rb
@@ -1,6 +1,5 @@
 class AddMapsData < ActiveRecord::Migration
   def change
     change_column(:maps, :private, :boolean, default: false)
-    Map.create(title: "臺灣Maker空間", kind: "google")
   end
 end
