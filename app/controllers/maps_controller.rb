0fec8fe6932bc08b07609dc0fb5403738e65e25b
KinoLien <miecowbai@gmail.com>
2015-08-12 19:58:42 +0800

    Add file input at map edit view. (not complete)

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -13,6 +13,7 @@ class MapsController < ApplicationController
 
   def edit
     @map = Map.find(params[:id])
+    @map.location_pins.build
   end
 
   def show
@@ -68,7 +69,7 @@ class MapsController < ApplicationController
   private
 
   def map_params
-    params.require(:map).permit(:title, :description, :private)
+    params.require(:map).permit(:title, :description, :private, :location_pins_attributes => [:id, :pin])
   end
 
   def assign_role(role_name)
